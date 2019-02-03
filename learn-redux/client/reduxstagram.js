3ee1645262e621066b3a659c2ecc13065a8dc34e
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2019-02-03 15:15:51 -0600

    add routes for render

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 import {render} from 'react-dom';
 
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+
 //import css
 import css from './styles/style.styl';
 
 //import components
 import Main from './Components/Main';
+import PhotoGrid from './Components/PhotoGrid';
+import Single from './Components/Single';
 
-render(<Main />, document.getElementById('root'));
+const router = (
+    <Router history={browserHistory}>
+        <Route path='/' component={Main}></Route>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path='/view/:postId' component={Single}></Route>
+    
+    </Router>
+)
+
+
+render(router, document.getElementById('root'));
